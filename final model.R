4a42ab4f239a7e1304ba8cb608641e98e1f87738
Kenshin <kenshin@Sakabato.(none)>
2014-05-14 17:03:22 -0400

    progress was made

--- final model.R
+++ final model.R
@@ -5,7 +5,7 @@
 
 library(KernSmooth)
 #library(cvTools)
-#use crossvalidation to evaluate the model, soon to come
+#TODO use crossvalidation to evaluate the model
 
 # use $y of the density function as input
 peaks.valleys = function(d)
@@ -18,24 +18,37 @@ peaks.valleys = function(d)
 peaks = function(pv) { return(pv[seq(1,length(pv), 2)]) }
 valleys = function(pv) { return(pv[seq(0,length(pv), 2)]) }
 
-train = function(ET, c=2)
+hotw.estimate = function(ET.new, ET, c=2)
 { # return the nth peak where n is 1+the number of valleys it's greater than
-  pvs = sapply( round(0:167/24, 3), function(hotw, ET. = ET.classroom)
+  pvs = sapply( round(0:167/24, 3), function(hotw, ET. = ET)
   {
-    d = density(ET.[round(ET.[,1]%%7,3)==round(hotw,3), 5], bw="SJ")
+    d = density(ET.[round(ET.[,1]%%7,3)==round(hotw,3), 3], bw="SJ")
     pv = peaks.valleys(d$y)
     return(d$x[pv])
   }
   )
   p = sapply(1:168, function(h) {return(peaks(pvs[[h]]))})
   v = sapply(1:168, function(h) {return(valleys(pvs[[h]]))})
-  return (  sapply(1:nrow(ET), 
-    function(i, ET.=ET, c.=c,p.=p,v.=v) 
-    {
-      #print(i)
-      pv.i = round(ET[i,4])+1
-      return(p.[[pv.i]][1+sum(ET.[i,c.]>v.[[pv.i]])])
-    } ))
+  
+  training = sapply( 0:floor(nrow(ET)/8760),
+    function(j){
+      this.year = j*365*24 + 1
+      next.year = (j+1)*365*24
+      return(
+        sapply(this.year:min(min(nrow(ET)-this.year,1), next.year), 
+        function(i, ET.=ET, c.=c,p.=p,v.=v)
+        {
+          pv.i = round(round(ET.[i,1]%%7, 3)*24) + 1
+          print(pv.i)
+          return(p.[[pv.i]][1+sum(ET.[i,c.]>v.[[pv.i]])])
+        } )
+    )}
+      
+  )
+  #print(training)
+  
+  #return the mode of each of them
+  return ( sapply(training, function(x) { return(as.numeric(names(sort(-table(x)))[1])) } ) )
 }
 
 #CONVENTION (note, the dot has no syntactic meaning, it's part of the name)
@@ -57,31 +70,17 @@ m.t = function(temp.new, ET)
 # predict 1st argument based on 2nd, y on x
 predict = function(ET.new, ET)
 {
+  f.occupancy.new = rep(0, nrow(ET.new))
   f.building.type = 1 # vector eventually for buildings
-  f.occupancy = rep(0, 365) #frequency * building-specific estimate based on building type
+  f.occupancy = rep(0, nrow(ET.new)) #frequency * building-specific estimate based on building type
   f.temp =  m.t(ET.new[,2], ET=ET)*f.building.type
-  f.hourly = hotw.estimate(ET.new[,1]%%7, ET)
-  return( f.temp + f.hourly )
-}  
-  #data frame with year month day hour# hour format as row name
+  resid.ET = cbind(ET[,1:2], ET[,3] - f.temp - f.occupancy )
+  resid.ET.new = cbind(ET.new[,1:2], ET.new[,3] - m.t(ET.new[,2], ET=ET)*f.building.type - f.occupancy.new )
+  f.hourly = hotw.estimate(resid.ET.new, resid.ET)
+  #print(f.temp)
+  return( f.temp + f.hourly + f.occupancy )
+}
+
+#data frame with year month day hour# hour format as row name
 readable.matrix = function(m, col.date=1) { return(data.frame(m[-col.date], row.names=sapply(m[,col.date], readable.date))) }
 readable.date = function(d) { return(paste(format(as.Date(d,origin="1970-01-01"), "%Y %B %d"), "hour#", round(24*(d - floor(d))))) }
-
-
-#pvs = sapply(round(0:167/24,3), function(hotw){ return( density(m[round(m[,1]%%7,3)==round(hotw,3), 2], bw="SJ") ) } )
-# c is column of the matrix m we want to estimate
-# hotw.estimate = function(hotw, c=2)
-# {
-#   n = nrow(m)
-#   results = vector("numeric", n) #categorical, which cluster it belongs to
-#   d = density(m[m[,1]%%7==round(hotw,3), c], bw="SJ")
-#   pv = peaks.valleys(d$y)
-#   peaks = pv[seq(1,length(pv), 2)]
-#   valleys = pv[seq(0,length(pv), 2)]
-#   #print(pv)
-#   for( i in 1:n )
-#   {
-#     results[i] = 1+sum(m[i,c]>d$y[valleys])
-#   }
-#   return(d$x[peaks[results]])
-# }
