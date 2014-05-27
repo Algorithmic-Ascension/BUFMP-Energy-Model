f284883c7b41e1a7c6eb078dc27a3b92a8a47531
AlgorithmicAscension <kenshin@Sakabato.(none)>
2014-05-27 00:43:24 -0400

    acquireTemperature was revised, it works, but only at ~15s per page, where each page is 1 day, may be slightly faster on better internet.
    TODO:
    for optimization, maybe switch from orderedList to checking
    line by line if hour has changes? I still want to keep the
    last recorded temperature at a datehour (because most
    observations are made at 54 min on the hour), whlie keeping
    an ordering of insertion

--- acquireTemperature.py
+++ acquireTemperature.py
@@ -1,19 +1,32 @@
 from urllib2 import urlopen
 from datetime import datetime, timedelta
+from sys import argv
+from collections import OrderedDict
 
-with open('acquireTemperature.csv', 'w+') as csv:
-	runner = datetime(2011,01,30)
-	anchor = datetime(2011,02,02)
-	m = []
+year=int(argv[1])
+
+with open(`year` + 'Temp.csv', 'w+') as csv:
+	runner = datetime(year,01,01)
+	anchor = datetime(year,01,03)
 	while (runner < anchor):
+		m = OrderedDict()
 		print 'downloading data for '+`runner.year`+'/'+`runner.month`+'/'+`runner.day`
 		rows = urlopen('http://www.wunderground.com/history/airport/KBOS/'+`runner.year`+'/'+`runner.month`+'/'+`runner.day`+'/DailyHistory.html?format=1').read().split('<br />\n')
 		#rows = [','.join([`runner.year`,`runner.month`,`runner.day`] + row.split(',')[0:2]) for row in rows[1:len(rows)-1]]
 		#for row in rows:
 		#	row.split(':')[0].split(',')
 		#print rows
-		[m.append([`runner.year`,`runner.month`,`runner.day`] + row.split(',')[:2]) for row in rows[1:len(rows)-1]]
-		for n in m:
-			n[3] = n[3].split(':')[0]
+		
+		#[m.append([`runner.year`,`runner.month`,`runner.day`] + row.split(',')[:2]) for row in rows[1:len(rows)-1]]
+		#for n in m:
+		#	n[3] = n[3].split(':')[0]
+		#runner += timedelta(days=1)
+
+		rows = rows[1:-1]
+		cells = [row.split(',') for row in rows]
+		for cell in cells:
+			m[ datetime.strptime(runner.strftime("%Y-%m-%d") + ' ' + cell[0], '%Y-%m-%d %I:%M %p').strftime('%Y-%m-%d %H')] = cell[1]
 		runner += timedelta(days=1)
-	csv.write('\n'.join([','.join(n) for n in m] + ['']))
+	
+		for n in m:
+			csv.write('\n'.join([n + ',' + m[n]] + ['']))
--- final model.R
+++ final model.R
@@ -7,16 +7,19 @@ library(KernSmooth)
 #library(cvTools)
 #TODO use crossvalidation to evaluate the model
 
-ET.SMG = as.vector(t(read.csv("~/Documents/Work/BUFMP-Energy-Model/Electricity.SMG.2011.csv", header=F, na.strings="No Record")))
-ET.SMG = append(ET.SMG, as.vector(t(read.csv("~/Documents/Work/BUFMP-Energy-Model/Electricity.SMG.2012.csv", header=F, na.strings="No Record"))))
-ET.SMG = append(ET.SMG, as.vector(t(read.csv("~/Documents/Work/BUFMP-Energy-Model/Electricity.SMG.2013.csv", header=F, na.strings="No Record"))))
-ET.SMG = append(ET.SMG, as.vector(t(read.csv("~/Documents/Work/BUFMP-Energy-Model/Electricity.SMG.2014.csv", header=F, na.strings="No Record"))))
+E.SMG = as.vector(t(read.csv("~/Documents/Work/BUFMP-Energy-Model/Electricity.SMG.2010.csv", header=F, na.strings="No Record")))
+E.SMG = append(ET.SMG, as.vector(t(read.csv("~/Documents/Work/BUFMP-Energy-Model/Electricity.SMG.2011.csv", header=F, na.strings="No Record"))))
+E.SMG = append(ET.SMG, as.vector(t(read.csv("~/Documents/Work/BUFMP-Energy-Model/Electricity.SMG.2012.csv", header=F, na.strings="No Record"))))
+E.SMG = append(ET.SMG, as.vector(t(read.csv("~/Documents/Work/BUFMP-Energy-Model/Electricity.SMG.2013.csv", header=F, na.strings="No Record"))))
+#ET.SMG = append(ET.SMG, as.vector(t(read.csv("~/Documents/Work/BUFMP-Energy-Model/Electricity.SMG.2014.csv", header=F, na.strings="No Record"))))
+ET.SMG.dates = as.vector(t( matrix(as.numeric(as.Date("2010-01-01")):as.numeric(as.Date("2012-12-31")), nrow=730, ncol=24) + t(matrix(0:23/24, nrow=24, ncol=730))))
 
-lastDuplicates(m)
-{ # last copy of duplicates (most minutes are 54)
-  v = sapply(1:nrow(m), function(i) {return(paste(m[i,1], m[,2], m[i,3], m[i,4]))} )
-  return (m[rev(!duplicated(rev(v))),])
-}
+#works, but not fast enough
+#lastDuplicates = function(m)
+#{ # last copy of duplicates (most minutes are 54)
+#  v = sapply(1:nrow(m), function(i) {return(paste(m[i,1], m[,2], m[i,3], m[i,4]))} )
+#  return (m[rev(!duplicated(rev(v)))])
+#}
 
 
 # use $y of the density function as input
@@ -30,34 +33,42 @@ peaks.valleys = function(d)
 peaks = function(pv) { return(pv[seq(1,length(pv), 2)]) }
 valleys = function(pv) { return(pv[seq(0,length(pv), 2)]) }
 
-hotw.estimate = function(ET.new, ET, c=2)
+train = function(ET.new, ET, pvs, c=2)
 { # return the nth peak where n is 1+the number of valleys it's greater than
-  pvs = sapply( round(0:167/24, 3), function(hotw, ET. = ET)
-  {
-    d = density(ET.[round(ET.[,1]%%7,3)==round(hotw,3), 3], bw="SJ")
-    pv = peaks.valleys(d$y)
-    return(d$x[pv])
-  })
-  p = sapply(1:168, function(h) {return(peaks(pvs[[h]]))})
-  v = sapply(1:168, function(h) {return(valleys(pvs[[h]]))})
+#   pvs = sapply( round(0:167/24, 3), function(hotw, ET. = ET)
+#   {
+#     d = density(ET.[round(ET.[,1]%%7,3)==round(hotw,3), 3], bw="SJ")
+#     pv = peaks.valleys(d$y)
+#     return(d$x[pv])
+#   })
+   p = sapply(1:168, function(h) {return(peaks(pvs[[h]]))})
+   v = sapply(1:168, function(h) {return(valleys(pvs[[h]]))})
   
-  training = sapply( 0:floor(nrow(ET)/8760),
-    function(j){
-      this.year = j*365*24 + 1
-      next.year = (j+1)*365*24
-      return(
-        sapply(this.year:min(nrow(ET), next.year), 
+  #training = sapply( 0:floor(nrow(ET)/8760),
+  #  function(j){
+  #    this.year = j*365*24 + 1
+  #    next.year = (j+1)*365*24
+  #    return(
+  #      sapply(this.year:min(nrow(ET), next.year), 
+    training = sapply(1:nrow(ET),
         function(i, ET.=ET, c.=c,p.=p,v.=v)
         {
           pv.i = round(round(ET.[i,1]%%7, 3)*24) + 1
-          print(pv.i)
+          #print(pv.i)
           return(p.[[pv.i]][1+sum(ET.[i,c.]>v.[[pv.i]])])
         } )
-    )}
+  #  )}
       
-  )  
+  #)
+  return(training)
   #return the mode of each of hoty
-  return ( sapply(training, function(x) { return(as.numeric(names(sort(-table(x)))[1])) } ) )
+  #return (
+    #unlist(
+  #    sapply(training, function(x) { 
+  #      return(as.numeric(names(sort(-table(x)))[1]))
+  #    } )
+    #)
+  #)
 }
 
 #CONVENTION (note, the dot has no syntactic meaning, it's part of the name)
@@ -85,8 +96,17 @@ predict = function(ET.new, ET)
   f.temp =  m.t(ET.new[,2], ET=ET)*f.building.type
   resid.ET = cbind(ET[,1:2], ET[,3] - f.temp - f.occupancy )
   resid.ET.new = cbind(ET.new[,1:2], ET.new[,3] - m.t(ET.new[,2], ET=ET)*f.building.type - f.occupancy.new )
+
+  #TODO integrate into hotw.estimate
+  pvs = sapply( round(0:167/24, 3), function(hotw, ET. = ET)
+  {
+    d = density(ET.[round(ET.[,1]%%7,3)==round(hotw,3), 3], bw="SJ")
+    pv = peaks.valleys(d$y)
+    return(d$x[pv])
+  })
+  
   f.hourly = hotw.estimate(resid.ET.new, resid.ET)
-  #print(f.temp)
+  #print(f.hourly)
   return( f.temp + f.hourly + f.occupancy )
 }
 
